5bc2fff3c640bf0341f20528de8c3f0a8a4b8df5
JonasMartins <jonas.s.martins@hotmail.com>
2016-06-09 21:21:02 -0300

    Test trivial model class Atividade

---
+++ MeuMedico/app/src/test/java/br/ufc/dc/es/meumedico/model/AtividadeTest.java
@@ -0,0 +1,65 @@
+package br.ufc.dc.es.meumedico.model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ufc.dc.es.model.Atividade;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jonas on 09/06/16.
+ */
+public class AtividadeTest {
+
+    private Atividade atividade;
+
+    @Before
+    public void setUp() throws Exception {
+
+        atividade = new Atividade();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetNome() throws Exception {
+
+        atividade.setNome("name");
+        assertEquals(atividade.getNome(),"name");
+
+    }
+
+    @Test
+    public void testGetDescricao() throws Exception {
+        atividade.setDescricao("something");
+        assertEquals(atividade.getDescricao(),"something");
+    }
+
+    @Test
+    public void testGetData() throws Exception {
+        atividade.setData("data");
+        assertEquals(atividade.getData(),"data");
+    }
+
+    @Test
+    public void testGetHora() throws Exception {
+        atividade.setHora("hora");
+        assertEquals(atividade.getHora(),"hora");
+    }
+    @Test
+    public void testGetId() throws Exception {
+        atividade.setId(12);
+        assertEquals(atividade.getId(),12);
+    }
+
+    @Test
+    public void testGetId_usuario() throws Exception {
+        atividade.setId(11);
+        assertEquals(atividade.getId(),11);
+    }
+
+}
