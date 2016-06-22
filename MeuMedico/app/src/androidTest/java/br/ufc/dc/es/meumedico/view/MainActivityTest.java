168208acd86473a6f4f77142badeeada9eec451e
JonasMartins <jonas.s.martins@hotmail.com>
2016-06-21 21:03:56 -0300

    Terminado test da view Login inicial

--- MeuMedico/app/src/androidTest/java/br/ufc/dc/es/meumedico/view/MainActivityTest.java
+++ MeuMedico/app/src/androidTest/java/br/ufc/dc/es/meumedico/view/MainActivityTest.java
@@ -2,6 +2,7 @@ package br.ufc.dc.es.meumedico.view;
 
 import com.robotium.solo.Solo;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
 
 /**
  * Created by jonas on 19/06/16.
@@ -29,10 +30,24 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         solo.finishOpenedActivities();
     }
 
+    @SmallTest
     public void testMainActivity() throws Exception {
         solo.enterText(0,"email@exampl.com");
         solo.typeText(1,"password");
+
+        solo.assertCurrentActivity("This isn't the right Activity to test!",MainActivity.class);
+        solo.assertMemoryNotLow();
+
         solo.searchText("email@exampl.com");
         solo.searchText("password");
+
+        solo.clearEditText(0);
+        solo.clearEditText(1);
+
+        /** The same as touch on some button or else */
+        solo.clickOnText("CADASTRE-SE");
+        solo.waitForDialogToOpen(5000);
+        solo.waitForDialogToClose(10000);
+
     }
 }
