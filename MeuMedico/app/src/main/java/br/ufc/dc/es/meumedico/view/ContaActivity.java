1f69c8dad9dc8e2ebfa28485707669da1017d188
Cesar Rodrigues <rasec1287@hotmail.com>
2016-07-03 15:45:41 -0300

    added somes validations to email and password

--- MeuMedico/app/src/main/java/br/ufc/dc/es/meumedico/view/ContaActivity.java
+++ MeuMedico/app/src/main/java/br/ufc/dc/es/meumedico/view/ContaActivity.java
@@ -1,10 +1,12 @@
 package br.ufc.dc.es.meumedico.view;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import br.ufc.dc.es.meumedico.controller.helper.ContaHelper;
@@ -17,11 +19,21 @@ public class ContaActivity extends AppCompatActivity {
 
     Button createAccount;
     ContaHelper helper;
+    private TextInputLayout emailTIL, passwordTIL;
+    private EditText nameET, emailET, passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conta);
+
+        nameET = (EditText) findViewById(R.id.CadastroNome);
+
+        emailTIL = (TextInputLayout) findViewById(R.id.contaInputUserEmail);
+        emailET = (EditText) findViewById(R.id.CadastroEmail);
+
+        passwordTIL = (TextInputLayout) findViewById(R.id.contaInputUserPassword);
+        passwordET = (EditText) findViewById(R.id.CadastroSenha);
 
         createAccount = (Button) findViewById(R.id.btcriarConta);
 
@@ -51,9 +63,9 @@ public class ContaActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 ValidacaoHelper vh = new ValidacaoHelper(ContaActivity.this);
-                if(vh.verificaCamposVaziosConta()){
+                if (vh.verificaCamposVaziosConta()) {
                     Toast.makeText(ContaActivity.this, "Todos os campos são obrigatórios", Toast.LENGTH_LONG).show();
-                }else {
+                }else if(validateEmail() && validatePassword()){
                     helper = new ContaHelper(ContaActivity.this);
                     final Login login = helper.pegaCadastroLogin();
 
@@ -67,7 +79,6 @@ public class ContaActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -77,5 +88,29 @@ public class ContaActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean validateEmail() {
+
+        String email = emailET.getText().toString();
+
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailTIL.requestFocus();
+            emailTIL.setError("Email inválido");
+            return false;
+        }else{
+            emailTIL.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        if(passwordET.getText().toString().length()<8){
+            passwordTIL.setError("Sua senha deve ter no mínimo 8 caracteres");
+            return false;
+        }else{
+            passwordTIL.setErrorEnabled(false);
+            return true;
+        }
     }
 }
